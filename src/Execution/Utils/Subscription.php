8eea6ad80d6a49502502a7e1b3ed23d605e2e291
Boris <39871739+Saarnaki@users.noreply.github.com>
2023-05-06 12:49:43 +0300

    Update Subscription.php

--- src/Execution/Utils/Subscription.php
+++ src/Execution/Utils/Subscription.php
@@ -13,29 +13,48 @@ class Subscription
     /** Broadcast subscription to client(s). */
     public static function broadcast(string $subscriptionField, mixed $root, ?bool $shouldQueue = null): void
     {
+        info('inside Subscription broadcast');
+        
         // Ensure we have a schema and registered subscription fields
         // in the event we are calling this method in code.
         $schemaBuilder = Container::getInstance()->make(SchemaBuilder::class);
         $schemaBuilder->schema();
+        
+        info('we have a schema');
 
         $registry = Container::getInstance()->make(SubscriptionRegistry::class);
         if (! $registry->has($subscriptionField)) {
             throw new \InvalidArgumentException("No subscription field registered for {$subscriptionField}");
         }
+        
+        info('registry set up');
 
         // Default to the configuration setting if not specified
         if ($shouldQueue === null) {
             $shouldQueue = config('lighthouse.subscriptions.queue_broadcasts', false);
         }
+        
+        info(['shouldQueue', $shouldQueue]);
+        info(['subscriptionField', $subscriptionField]);
 
         $subscription = $registry->subscription($subscriptionField);
+        
+        info('subscription set up');
+        
         $broadcaster = Container::getInstance()->make(BroadcastsSubscriptions::class);
+        
+        info('broadcaster set up');
 
         try {
+            info('inside try');
             if ($shouldQueue) {
+                info('inside shouldQueue');
                 $broadcaster->queueBroadcast($subscription, $subscriptionField, $root);
+                info('queueBroadcast executed');
             } else {
+                info('inside else');
                 $broadcaster->broadcast($subscription, $subscriptionField, $root);
+                info('broadcast executed');
             }
         } catch (\Throwable $throwable) {
             $exceptionHandler = Container::getInstance()->make(SubscriptionExceptionHandler::class);
